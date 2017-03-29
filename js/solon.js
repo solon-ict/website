0c13ce2326ce85394964b489a70d8f803b13c08b
Nivade <nhavandeursen@gmail.com>
2017-03-29 06:35:11 +0200

    add client page, remove fullpage, animate navbar, add lib folder, add solon.js

---
+++ js/solon.js
@@ -0,0 +1,39 @@
+
+
+
+// Initialize fullpage.
+/* $(document).ready(function() {
+  $('#fullpage').fullpage({
+    menu: '#menu',
+    anchors:['home', 'diensten', 'portfolio', 'over', 'contact', 'footer'],
+    autoScrolling: false,
+    fitToSection: false,
+    paddingTop: '50px',
+		fixedElements: '#menu',
+    scrollBar: false
+  });
+}); */
+
+$(document).on('click', 'a.page-scroll', function(event) {
+    var $anchor = $(this);
+    $('html, body').stop().animate({
+        scrollTop: ($($anchor.attr('href')).offset().top - 50)
+    }, 1250, 'easeInOutExpo');
+    event.preventDefault();
+});
+
+// Make the navbar collapse when a link is clicked.
+$(document).on('click','.navbar-collapse.in',function(e) {
+    if( $(e.target).is('a') ) {
+        $(this).collapse('hide');
+    }
+});
+
+// Alter the navbar after scrolling past the landing page.
+$(window).scroll(function() {
+    if ($("#navigation").offset().top > 100) {
+        $("#navigation").addClass("navbar-shrink");
+    } else {
+        $("#navigation").removeClass("navbar-shrink");
+    }
+});
